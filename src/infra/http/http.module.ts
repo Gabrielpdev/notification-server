36b7110818d47ee2d276508e430452fdffb68f5e
Gabriel Oliveira <gabriel9938@gmail.com>
2022-12-15 18:16:43 -0300

    fix: ajust http controllers

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,3 +1,8 @@
+import { CancelNotification } from '@application/use-cases/cancel-notifications';
+import { CountRecipientNotification } from '@application/use-cases/count-recipient-notifications';
+import { GetRecipientNotification } from '@application/use-cases/get-recipient-notifications';
+import { ReadNotification } from '@application/use-cases/read-notifications';
+import { UnreadNotification } from '@application/use-cases/unread-notifications';
 import { Module } from '@nestjs/common';
 import { SendNotification } from 'src/application/use-cases/send-notifications';
 import { DatabaseModule } from '../database/database.module';
@@ -6,6 +11,13 @@ import { NotificationsController } from './controllers/notifications.controller'
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationsController],
-  providers: [SendNotification],
+  providers: [
+    SendNotification,
+    CancelNotification,
+    CountRecipientNotification,
+    GetRecipientNotification,
+    ReadNotification,
+    UnreadNotification,
+  ],
 })
 export class HttpModule {}
