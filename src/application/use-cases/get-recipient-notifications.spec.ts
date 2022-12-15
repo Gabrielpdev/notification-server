8bd2ecc6048f28aef0d036cea0fbde296dd6b652
Gabriel Oliveira <gabriel9938@gmail.com>
2022-12-15 17:39:18 -0300

    feat: list by recipient id

---
+++ src/application/use-cases/get-recipient-notifications.spec.ts
@@ -0,0 +1,42 @@
+import { makeNotification } from '@test/factories/notification-factory';
+import { InMemoryNotificationsRepository } from '../../../test/repositories/in-memory-notifications-repository';
+import { GetRecipientNotification } from './get-recipient-notifications';
+
+describe('Get notification', () => {
+  it('should be able to count recipient notifications', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const contRecipientNotification = new GetRecipientNotification(
+      notificationsRepository,
+    );
+
+    await notificationsRepository.create(
+      makeNotification({
+        recipientId: 'exempla-uuid-1',
+      }),
+    );
+
+    await notificationsRepository.create(
+      makeNotification({
+        recipientId: 'exempla-uuid-1',
+      }),
+    );
+
+    await notificationsRepository.create(
+      makeNotification({
+        recipientId: 'exempla-uuid-2',
+      }),
+    );
+
+    const { notifications } = await contRecipientNotification.execute({
+      recipientId: 'exempla-uuid-1',
+    });
+
+    expect(notifications).toHaveLength(2);
+    expect(notifications).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ recipientId: 'exempla-uuid-1' }),
+        expect.objectContaining({ recipientId: 'exempla-uuid-1' }),
+      ]),
+    );
+  });
+});
